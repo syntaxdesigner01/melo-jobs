7dd6134f66c6370d050b651e619c2f4b52cb7f22
syntaxdesigner01 <syntaxdesigners@gmail.com>
2024-01-25 01:51:06 +0100

    started project

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,8 @@
+
+export default function Header() {
+  return (
+    <div className="bg-blue-600 text-white h-20 ">
+        <nav className="container  flex items-center justify-center w-full h-full text-3xl font-bold">Melo Jobs</nav>
+    </div>
+  )
+}
